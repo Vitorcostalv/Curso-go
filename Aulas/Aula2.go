ece0ce67ac69d15f2afc564ccef0a3495ba3611c
Vitorcostalv <vitorcosta99900@gamil.com>
2024-10-23 21:47:34 -0300

    Declaração de variáveis, tipos de dados básicos (int, float, string, bool), constantes.

---
+++ Aulas/Aula2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    var age int = 25
+    var name string = "Vitor"
+    const pi float64 = 3.14159
+    isStudent := true
+
+    fmt.Println("Name:", name)
+    fmt.Println("Age:", age)
+    fmt.Println("PI:", pi)
+    fmt.Println("Is Student:", isStudent)
+}
